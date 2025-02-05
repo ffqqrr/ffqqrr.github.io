03d2218388110d914fd4691a703719ada6debbf0
fqrfqr <fqrqwq@qq.com>
2025-02-05 21:55:08 +0800

    Site updated: 2025-02-05 21:55:08

---
+++ js/smooth-scroll.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const links = document.querySelectorAll('a[href^="#"]');
+  
+    links.forEach(link => {
+      link.addEventListener("click", function (e) {
+        e.preventDefault();
+  
+        const targetId = this.getAttribute("href").substring(1);
+        const targetElement = document.getElementById(targetId);
+  
+        if (targetElement) {
+          targetElement.scrollIntoView({ behavior: "auto", block: "start" });
+        }
+      });
+    });
+  });
+  
